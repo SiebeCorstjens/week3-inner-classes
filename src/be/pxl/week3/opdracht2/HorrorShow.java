68c3c987bf01dc04e1c0c68a9aeda6d8695c34df
SiebeCorstjens <siebe.corstjens@student.pxl.be>
2017-10-16 10:54:10 +0200

    Voor mijn broer

--- src/be/pxl/week3/opdracht2/HorrorShow.java
+++ src/be/pxl/week3/opdracht2/HorrorShow.java
@@ -5,28 +5,36 @@ public class HorrorShow {
 	public static void main(String[] args) {
 		class Vampire implements Monster {
 			@Override
-			public String menace() {
-				return "I'll drink your blood!!";
+			public void menace() {
+				System.out.println("I'll drink your blood!!");
 			}
 		}
 		
 		class Werewolf implements DangerousMonster {
 			@Override
-			public String menace() {
-				return "I'll destroy you.";
+			public void menace() {
+				System.out.println("I'll destroy you.");
 			}
 			
 			@Override
 			public void destroy(Monster monster) {
-				System.out.println(menace() + " Big fight " + getClass().getSimpleName() + " killed one " + monster.getClass().getSimpleName());
+				monster.menace();
+				System.out.println(" Big fight ...\n" + getClass().getSimpleName() + " killed one " + monster.getClass().getSimpleName());
 			}
 		}
 		
 		Werewolf werewolf = new Werewolf();
-		werewolf.destroy(new Vampire());
+		werewolf.menace();
 		
+		Vampire vampire = new Vampire();
+		vampire.menace();
+		
+		werewolf.destroy(new Monster() {
+			
+			@Override
+			public void menace() {
+				System.out.println("You smell like wet dog.");
+			}
+		});
 	}
-	
-	
-
 }
